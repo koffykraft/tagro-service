6de3b4aa3ebe1b77dfabd0d50226c6dfb708fb20
Thomas <info@tagro.in>
2026-09-01 16:48:45 +0530

    Revert "Rebuild service app operating shell"
    This reverts commit 6d46015dbc1d8e991a4bb04b96d1400628660599.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tagro-v10-new-os-shell';
+const CACHE_NAME = 'tagro-v9-billing-trace';
 const ASSETS = [
   '/',
   '/index.html',
@@ -8,8 +8,9 @@ const ASSETS = [
   '/tracker.html',
   '/reference.html',
   '/app.js',
-  '/tagro-ui.css',
-  '/tagro-os.js',
+  '/app.css',
+  '/service-desk.css',
+  '/service-desk.js',
   '/parts-search.js',
   '/manifest.json'
 ];
